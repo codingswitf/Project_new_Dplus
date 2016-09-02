8f001c1138022e09024e5cb5c355f0a1300600e3
codingswitf <wutthiphorn.coding@gmail.com>
2016-09-02 18:37:29 +0700

    f
    f

--- json_sameple.php
+++ json_sameple.php
@@ -9,13 +9,24 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT * FROM `register_insuren` LIMIT 0, 30 ";
+// $email = $_POST["email"];
+// $First_name = $_POST["First_name"];
+// $last_name = $_POST["last_name"];
+// $address = $_POST["address"];
+// $city = $_POST["city"];
+// $postal_code = $_POST["postal_code"];
+// $phone = $_POST["phone"];
+$email = "email";
+$First_name = "First_name";
+$last_name = "last_name";
+$address = "address";
+$city = "city";
+$postal_code = "postal_code";
+$phone = "phone";
+$sql = "INSERT INTO  `sql6117117`.`register_insuren` (`id` ,`email` ,`First_name` ,`last_name` ,`address` ,`city` ,`postal_code` ,`phone`)
+VALUES (NULL ,  '+$email+',  '+$First_name+',  '+$last_name+',  '+$address+',  '+$city+',  '+$postal_cod+',  '+phone+');"
+
 $conn->query("SET names utf8");
-$myArray = array();
 $result = $conn->query($sql);
-  while($row = $result->fetch_array(MYSQL_ASSOC)){
-      $myArray[]=$row;
-  }
-echo json_encode($myArray,JSON_UNESCAPED_UNICODE);
 $conn->close();
 ?>	
